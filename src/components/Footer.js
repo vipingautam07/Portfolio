763d83d9f4b12c8c318186816722cb5494eb9e1f
Vipin Gautam <gautamvipin2002@gmail.com>
2023-07-21 02:29:13 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+            <p>
+              gautamvipin2002@gmail.com
+            </p>
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/vipingautam07/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://twitter.com/vipingautam07_/"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://instagram.com/vipingautam07_/"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://github.com/vipingautam07/"><img src={navIcon4} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2023. All Rights Reserved to VIPIN.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,75 @@
+import meter1 from "../assets/img/c++.png";
+import meter2 from "../assets/img/react.png";
+import meter3 from "../assets/img/js.png";
+import meter4 from "../assets/img/nodejs.png";
+import meter5 from "../assets/img/merge.png";
+import meter6 from "../assets/img/wordpress.png";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+// import arrow1 from "../assets/img/arrow1.svg";
+// import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>Skills in the field of software engineering are the specific abilities, knowledge, and proficiencies that enable professionals to design, develop, test, and maintain software systems effectively. <br></br> Data Structures & Algorithms, OOPs, Operating Systems, Database Management System</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h3>C++</h3>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>React.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Javascript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter4} alt="Image" />
+                                <h5>Node.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter5} alt="Image" />
+                                <h5>Git & Github</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter6} alt="Image" />
+                                <h5>Wordpress</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img className="background-image-left" src={colorSharp} alt="Image" />
+    </section>
+  )
+}
