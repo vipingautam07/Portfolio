c18f35c28b190463e89ac6fbd968cc77b7c65815
Vipin Gautam <95081432+vipingautam07@users.noreply.github.com>
2023-08-26 12:03:40 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://portfolio-contact-db2c8-default-rtdb.firebaseio.com/userRecord.json", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
