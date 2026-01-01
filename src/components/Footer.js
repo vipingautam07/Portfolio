dc9dfc558522c28843654d576c6a4362ea76ae74
Vipin Gautam <gautamvipin2002@gmail.com>
2026-01-01 12:40:41 +0530

    adding the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,6 +25,12 @@ export const Footer = () => {
               <a href="https://leetcode.com/vipingautam07/"><img src={navIcon3} alt="Icon" /></a>
               <a href="https://github.com/vipingautam07/"><img src={navIcon4} alt="Icon" /></a>
             </div>
+            <a href="https://www.buymeacoffee.com/vipingautam" target="_blank" rel="noopener noreferrer" className="buy-coffee-btn">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.89 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4v-2z"/>
+              </svg>
+              <span>Buy Me a Coffee</span>
+            </a>
             <p>Copyright 2026. All Rights Reserved to VIPIN.</p>
           </Col>
         </Row>
